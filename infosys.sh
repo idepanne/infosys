694739d680837dcf189ea1416010ed30a19523fb
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-02-11 21:59:33 +0100

    Add files via upload

--- infosys.sh
+++ infosys.sh
@@ -45,8 +45,8 @@ fi
 var18=$(uptime -s)
 var19=$(uptime -p)
 var20=$(ls /usr/bin/*session)
-var21=$(/sbin/ip route show dev eth0 | grep default | cut -c13- | rev | cut -c30- | rev)
-var22=$(/sbin/ip -6 route show dev eth0 | grep default | cut -c13- | rev | cut -c42- | rev)
+var21=$(/sbin/ip route show dev eth0 | grep default)
+var22=$(/sbin/ip -6 route show dev eth0 | grep default)
 ######################################
 
 echo ""
@@ -84,7 +84,10 @@ echo -n "EEPROM          : "
 sudo rpi-eeprom-update
 echo ""
 echo -n "IPv4/IPv6       : "; hostname -I
-echo -n "Routeur         : "; echo "$var21 $var22"
+echo ""
+echo "Routeur         : "
+echo "$var21"
+echo "$var22"
 echo ""
 echo -n "Nom d'hôte      : "; hostname
 echo ""
