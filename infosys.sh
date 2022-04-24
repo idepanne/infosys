2585cd6c6b9d9caf60ad35228e59af8730caabc4
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-04-24 21:18:38 +0200

    Add files via upload

--- infosys.sh
+++ infosys.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # infosys.sh
-# [78]
+# [79]
 # Informations système pour Linux et Raspberry Pi
 # © 2020-2022 iDépanne – L'expert informatique
 # idepanne67@gmail.com
 
 cd
-sudo apt update && sudo apt install -y neofetch inxi
+sudo apt update && sudo apt install -y neofetch inxi zram-tools
 mkdir .config
 cd .config
 mkdir neofetch
