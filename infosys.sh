986204d1994d4363cf0eb67ab92e8c4ee69e49db
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-08-17 19:00:38 +0200

    Add files via upload

--- infosys.sh
+++ infosys.sh
@@ -36,9 +36,8 @@ if [[ $var0 == *"Raspberry Pi"* ]]; then
 	var8=$(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq rev | cut -c4- | rev)
 	var9=$(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq rev | cut -c4- | rev)
 	var10=$(vcgencmd measure_volts core | cut -c6-)
-	#var11=$(vcgencmd get_config int | grep -E "(gpu_freq)" | cut -c10-)
-	#var12=$(echo "$var11" | rev | cut -c9- | rev)
-    var11=$(vcgencmd get_config int | grep -E "(gpu_freq)")
+	var11=$(vcgencmd get_config int | grep -E "(gpu_freq)" | cut -c10-)
+	var12=$(echo "$var11" | rev | cut -c9- | rev)
     var13=$(uname -srv)
 	#var14=$(< /etc/os-release grep PRETTY_NAME | cut -c14-)
     var14=$(< /etc/os-release grep PRETTY_NAME | cut -c14- | rev | cut -c2- | rev)
@@ -74,7 +73,7 @@ if [[ $var0 == *"Raspberry Pi"* ]]; then
 	echo -n "Température     : "; echo "$(vcgencmd measure_temp | grep -E -o '[0-9]*\.[0-9]*')°C"
 	echo ""
 	echo -n "GPU RAM         : "; echo "$(vcgencmd get_mem gpu)" | cut -c5-
-    echo -n "GPU fréquences  : "; echo $var11
+    echo -n "GPU fréquences  : "; echo $var12
 	echo -n "Codec H264      : "; echo "$(vcgencmd codec_enabled H264)" | cut -c6-
 	echo -n "Codec H265      : "; echo "$(vcgencmd codec_enabled H265)" | cut -c6-
 	echo ""
