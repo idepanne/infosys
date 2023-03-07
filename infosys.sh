9b434d8faecedaeaf263ec701a3f2e1e0240a7fb
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-03-07 01:22:04 +0100

    Add files via upload

--- infosys.sh
+++ infosys.sh
@@ -2,7 +2,7 @@
 #echo "+=============================================================================+"
 #echo "|       Infos système pour distributions Linux basées sur Debian ou Arch      |"
 #echo "|                                 infosys.sh                                  |"
-#echo "|                                   [140]                                     |"
+#echo "|                                   [142]                                     |"
 #echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 #echo "|                            idepanne67@gmail.com                             |"
 #echo "+=============================================================================+"
