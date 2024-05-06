9d5e788b9d62b878c8336f00b816b2d6bfaa741c
iDépanne <69732672+idepanne@users.noreply.github.com>
2024-05-06 16:12:00 +0200

    Add files via upload

--- infosys-rpi.sh
+++ infosys-rpi.sh
@@ -2,7 +2,7 @@
 #echo "+=============================================================================+"
 #echo "|             Informations système pour Raspberry Pi OS uniquement            |"
 #echo "|                              infosys-rpi.sh                                 |"
-#echo "|                                   [179]                                     |"
+#echo "|                                   [180]                                     |"
 #echo "|                © 2019-2024 iDépanne – L'expert informatique                 |"
 #echo "|                        idepanne.support.tech@free.fr                        |"
 #echo "+=============================================================================+"
