b9a62d53b6a3ff401f5dcbd243b8298ea7680d6c
iDépanne <69732672+idepanne@users.noreply.github.com>
2024-05-29 21:41:30 +0200

    Add files via upload

--- apps/infosys_test.sh
+++ apps/infosys_test.sh
@@ -2,7 +2,7 @@
 #echo "+=============================================================================+"
 #echo "|   Infos système pour distributions Linux basées sur Debian, Arch ou Fedora  |"
 #echo "|                                 infosys.sh                                  |"
-#echo "|                                   [190]                                     |"
+#echo "|                                   [191]                                     |"
 #echo "|                © 2019-2024 iDépanne – L'expert informatique                 |"
 #echo "|                        idepanne.support.tech@free.fr                        |"
 #echo "+=============================================================================+"
@@ -17,17 +17,17 @@ varsys=$(< /etc/os-release)
 ######################################
 
 if [[ $varsys == *"arch"* || $varsys == *"manjaro"* || $varsys == *"endeavouros"* ]]; then
-	echo "Arch"
+	echo "Distribution mère : Arch Linux"
 	echo ""
 	sudo pacman -S --needed --noconfirm fastfetch inxi inetutils ; sudo pacman -Rsn --noconfirm neofetch ; sudo rm -rv ~/.config/neofetch
 else
 	if [[ $varsys == *"fedora"* ]]; then
-		echo "Fedora"
+		echo "Distribution mère : Fedora Linux"
 		echo ""
 		#sudo dnf
 	else
 		if [[ $varsys == *"Debian"* ]]; then
-			echo "Debian"
+			echo "Distribution mère : Debian Linux"
 			echo ""
 			sudo apt-get update ; sudo apt-get install -y inxi smartmontools ; sudo apt-get purge -y neofetch ; sudo rm -rv ~/.config/neofetch
 			var55=$(fastfetch --version)
