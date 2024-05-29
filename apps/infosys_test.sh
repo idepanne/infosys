05d4391e12a2891a75f607ec095794673cb08eee
iDépanne <69732672+idepanne@users.noreply.github.com>
2024-05-29 22:08:16 +0200

    Add files via upload

--- apps/infosys_test.sh
+++ apps/infosys_test.sh
@@ -2,7 +2,7 @@
 #echo "+=============================================================================+"
 #echo "|   Infos système pour distributions Linux basées sur Debian, Arch ou Fedora  |"
 #echo "|                                 infosys.sh                                  |"
-#echo "|                                   [192]                                     |"
+#echo "|                                   [193]                                     |"
 #echo "|                © 2019-2024 iDépanne – L'expert informatique                 |"
 #echo "|                        idepanne.support.tech@free.fr                        |"
 #echo "+=============================================================================+"
@@ -34,12 +34,27 @@ else
 			echo ""
 			sudo apt-get update ; sudo apt-get install -y inxi smartmontools ; sudo apt-get purge -y neofetch ; sudo rm -rv ~/.config/neofetch
 			var55=$(fastfetch --version)
-			if [[ "$var55" =~ "fastfetch 2.13.2 (aarch64)" ]]; then
+			if [[ "$var55" =~ "fastfetch 2.13.2 (amd64)" ]]; then
 				echo ""
 			else
 				echo ""
-				echo "cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-aarch64.deb > fastfetch-linux-aarch64.deb && sudo dpkg -i fastfetch-linux-aarch64.deb ; sudo rm -rv fastfetch-linux-aarch64.deb"
-				cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-aarch64.deb > fastfetch-linux-aarch64.deb && sudo dpkg -i fastfetch-linux-aarch64.deb ; sudo rm -rv fastfetch-linux-aarch64.deb
+				echo "cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-amd64.deb > fastfetch-linux-amd64.deb && sudo dpkg -i fastfetch-linux-amd64.deb ; sudo rm -rv fastfetch-linux-amd64.deb"
+				cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-amd64.deb > fastfetch-linux-amd64.deb && sudo dpkg -i fastfetch-linux-amd64.deb ; sudo rm -rv fastfetch-linux-amd64.deb
+			fi
+		else
+			if [[ $varsys == *"Raspberry"* || $varsys == *"raspberry"* ]]; then
+				echo ""
+				echo "Distribution mère : Debian Linux"
+				echo ""
+				sudo apt-get update ; sudo apt-get install -y inxi smartmontools ; sudo apt-get purge -y neofetch ; sudo rm -rv ~/.config/neofetch
+				var55=$(fastfetch --version)
+				if [[ "$var55" =~ "fastfetch 2.13.2 (aarch64)" ]]; then
+					echo ""
+				else
+					echo ""
+					echo "cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-aarch64.deb > fastfetch-linux-aarch64.deb && sudo dpkg -i fastfetch-linux-aarch64.deb ; sudo rm -rv fastfetch-linux-aarch64.deb"
+					cd || return && wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.13.2/fastfetch-linux-aarch64.deb > fastfetch-linux-aarch64.deb && sudo dpkg -i fastfetch-linux-aarch64.deb ; sudo rm -rv fastfetch-linux-aarch64.deb
+				fi
 			fi
 		fi
 	fi
